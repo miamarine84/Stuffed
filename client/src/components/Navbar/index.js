03f0907138d8c16c6eee3af8d5e36bd9d8222e01
Rusky <jpah.design@gmail.com>
2020-11-25 21:04:21 -0500

    progress

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,7 +7,7 @@ import "./style.css";
 function Navbar() {
   return (
     
-    <nav className="navContainer navbar navbar-expand-lg">
+   /* <nav className="navContainer navbar navbar-expand-lg">
       <Link className="navbar-brand" to="/">
       <img src="https://user-images.githubusercontent.com/65417908/99906944-e5b5f580-2ca7-11eb-9582-a2313fa43539.png" width="70" height="70
       " alt="" loading="lazy"/>
@@ -53,9 +53,69 @@ function Navbar() {
     </Link>
     </nav>
     
-   
-   
-  );
+   */
+
+
+   /*NAV OPTION 2
+  <nav id="nav-wrap">
+
+  <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+ <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+  <ul id="nav" className="nav">
+  <Link className="navbar-brand" to="/">
+      <img src="https://user-images.githubusercontent.com/65417908/99906944-e5b5f580-2ca7-11eb-9582-a2313fa43539.png" width="70" height="70
+      " alt="" loading="lazy"/>
+      </Link>
+     <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+     <li><a className="smoothscroll" href="#dashboard"><Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/dashboard"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+     </Link> Dashboard</a></li>
+     <li><a className="smoothscroll" href="#matched"><Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/matched"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+     </Link> Matched</a></li>
+     <li><a className="smoothscroll" href="#logIn"><Link
+              to="/"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/LogIn"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+     </Link> LogIn</a></li>
+  </ul>
+
+</nav>
+  );*/
+
+  <nav id="nav-wrap">
+
+         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" className="nav">
+            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+            <li><a className="smoothscroll" href="#matched">matched</a></li>
+	         <li><a className="smoothscroll" href="#search">search</a></li>
+            <li><a className="smoothscroll" href="#logout">log out</a></li>
+            
+         </ul>
+
+      </nav>
+     
+        );
 }
 
 export default Navbar;
