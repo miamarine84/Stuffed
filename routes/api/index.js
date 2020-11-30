1f36681d791b9bd23dfe7673bc9293b6a2580e47
marco p <miamarine84@yahoo.com>
2020-11-30 18:26:08 -0500

    got login and signup working make sure to npm i jsonwebtoken

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,12 +2,13 @@ const router = require("express").Router();
 //const postRoutes = require("./posts");
 //const authRoutes = require("./authorize");
 
-
+const authRoutes = require("./auth");
 const accountRoutes = require("./account");
 
 // Post routes
 //router.use("/posts", postRoutes);
 //router.use("/auth", authRoutes);
 router.use("/account", accountRoutes);
+router.use("/auth", authRoutes);
 
 module.exports = router;
