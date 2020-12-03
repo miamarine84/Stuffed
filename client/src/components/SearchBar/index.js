a632afa55901fc55e1185aef270e430046aab6e3
Caitlin White <caitlinwhite@Caitlins-MacBook-Air.local>
2020-12-02 20:36:01 -0500

    search bar

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -28,7 +28,7 @@ export function SearchBar(props) {
                 <div className="control">
                     <div className={`button is-static ${sizeClass}`}></div>
                 </div>
-                <p className="control">
+                <p className="control">Where?
                     <input className={`form-control input ${sizeClass} ${styles['input-control']}`}
                            onChange={(e) => setLocation(e.target.value)}
                            type="text"
