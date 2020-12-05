451d570ab932768649fefb58cdd37a210b6731c9
marco p <miamarine84@yahoo.com>
2020-12-05 09:43:24 -0500

    private/public routes working as well as navbar links working properly

--- client/src/containers/SignUp/index.js
+++ client/src/containers/SignUp/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import SignUp from '../../components/SignUp';
 import {useStoreContext} from '../../utils/GlobalState';
 import {SIGN_UP} from '../../utils/actions';
+import Navbar from '../../components/Navbar';
 
 
 const SignUpPage=()=>{
     const [state,dispatch]=useStoreContext();
-    return <div>Sign Up Page
-
+    return <div>
+            <Navbar />
             <SignUp onSignUp={(account)=>{
                 dispatch({
                     type:SIGN_UP,
