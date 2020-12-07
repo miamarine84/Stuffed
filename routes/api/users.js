e3063176307a23269710ff82d81a17b0087c8c19
Miguelangel Weill <miguelangel.weill@gmail.com>
2020-12-07 11:19:43 -0500

    This is the search users working

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-//This is the route to set a liked restaurant
-router.route("/liked").put(userController.liked);
 //This is the route to find the users
 router.route("/search").get(userController.searchUser);
 
