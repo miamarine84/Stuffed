18696d2c4b49af27b377bb4aacbb9174b8b6283c
Miguelangel Weill <miguelangel.weill@gmail.com>
2020-12-09 19:04:00 -0500

    These are my changes

---
+++ routes/api/userInfo.js
@@ -0,0 +1,7 @@
+// const router = require("express").Router();
+// const userController = require("../../controllers/userController");
+
+// //This is the route to find the users
+// router.route("/currentUser").get(userController.userInfo);
+
+// module.exports = router;
